fa25bf26cc25825b6b8cbba76b4e37df85d14ef3
柊 <KTC2243030009@edu.kyoto-tech.ac.jp>
2024-08-06 04:02:12 +0900

    レコードの表示

--- django_app/PlaylistMaker/views.py
+++ django_app/PlaylistMaker/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import video_info
 
 # Create your views here.
 
 def index(request):
-  return render(request, 'PlaylistMaker/index.html')
+  data = video_info.objects.all()
+  params = {
+    'title': 'video info',
+    'message': '動画一覧',
+    'data': data,
+  }
+
+  return render(request, 'PlaylistMaker/index.html',params)
