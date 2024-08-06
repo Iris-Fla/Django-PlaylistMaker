9d999811d107fd6256d5dfeaedf22cf8e333e2c3
Meru🔮 <irith.fla@gmail.com>
2024-08-06 19:16:27 +0900

    modelの修正等

--- django_app/PlaylistMaker/models.py
+++ django_app/PlaylistMaker/models.py
@@ -1,20 +1,26 @@
 from django.db import models
 
+"""
+videosテーブル
+videoid: 動画ID
+title: 動画タイトル
+channel: チャンネル名
+image: サムネイル画像のURL
+url: 動画のURL
+views: 再生回数
+published_date: 公開日
+"""
+
 # Create your models here.
 class video_info(models.Model):
-  # name = models.CharField(max_length=100)
-  # mail = models.EmailField(max_length=200)
-  # gender = models.BooleanField()
-  # age = models.IntegerField(default=0)
-  # birthday = models.DateField()
+  videoid = models.CharField(max_length=100) #idは文字列なのでCharFieldを使った
   title = models.CharField(max_length=100) #titleとchannelは文字列なのでCharFieldを使った
   channel = models.CharField(max_length=100)
-  image = models.ImageField(width_field=image_width) #ImageFieldの使い方がこれであってるか分からん　一応画像の保存に使うっぽい
+  image = models.CharField(max_length=200) #画像のURLを保存するフィールド
   url = models.URLField() #URlのみを保存するフィールド
   views = models.PositiveBigIntegerField() #再生回数は正の数かつ最も再生されている動画が100億ぐらいなのでこのフィールド
   published_date = models.DateTimeField(null=True, blank=True) #Datetimeを保存するやつ　これであってるか分からん
 
 
-  def __str__(self): #オブジェクトを表示する際の出力形式をカスタマイズ
-    return '<video:id=' + str(self.id) + ',' + \
-      self.title + '(' + str(self.url) + ')>' #IDとtitleとurlを表示するためのメソッド
+  def __str__(self):
+    return '<videos:id=' + str(self.id) + ', title=' + self.title + ', channel=' + self.channel + ', image=' + self.image + ', url=' + self.url + ', views=' + str(self.views) + ', published_date=' + str(self.published_date) + '>'
