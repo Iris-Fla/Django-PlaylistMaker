feaa68abb9153d7cfd8c95fa05b0ac14fa5ba0b1
柊 <KTC2243030009@edu.kyoto-tech.ac.jp>
2024-08-07 01:41:28 +0900

    Revert "データ見る画面"
    This reverts commit e6207c8666ca2af7e2cc7c8730df8d1c8af5b721.

--- django_app/PlaylistMaker/views.py
+++ django_app/PlaylistMaker/views.py
@@ -4,11 +4,11 @@ from .models import video_info
 # Create your views here.
 
 def index(request):
-  data = video_info.objects.all()
-  params = {
-    'title': 'video info',
-    'message': '動画一覧',
-    'data': data,
-  }
+  # data = video_info.objects.all()
+  # params = {
+  #   'title': 'video info',
+  #   'message': '動画一覧',
+  #   'data': data,
+  # }
 
   return render(request, 'PlaylistMaker/index.html',params)
