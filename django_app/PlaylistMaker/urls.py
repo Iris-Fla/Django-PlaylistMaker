0494a124aec2c5a5e170ce63598a7751a18863e7
Meru🔮 <irith.fla@gmail.com>
2024-08-11 10:05:21 +0900

    indexページ完成

--- django_app/PlaylistMaker/urls.py
+++ django_app/PlaylistMaker/urls.py
@@ -6,6 +6,5 @@ urlpatterns = [
   path('create', views.create, name='create'),
   path('videos/', views.video_list, name='video_list'),
   path('selected-videos/', views.selected_videos, name='selected_videos'),
-  path('create_playlist/', views.create_youtube_playlist, name='create_youtube_playlist'),
-  path('playlist/<str:playlist_id>/', views.playlist_detail, name='playlist_detail'),
+  path('create-playlist/', views.create_youtube_playlist, name='create_youtube_playlist'),
 ]
